a4cb826d17621023ae46388a116db742adf4e176
derinstaboy <90578108+derinstaboy@users.noreply.github.com>
2024-04-04 17:17:28 +0200

    Update Redirects.ts

--- components/Nav/Redirects.ts
+++ components/Nav/Redirects.ts
@@ -4,7 +4,7 @@ const reDirects = [
     { label: "Buy VOTG NFT", href: "https://opensea.io/collection/votonx-generation", subpages: ["/"] },
     { label: "Swap", href: "/Swap", subpages: ["/"] },
     { label: "Chart", href: "/Chart", subpages: ["/"] },
-    { label: "Contract", href: "https://polygonscan.com/token/0x3d1b34db20dd01A5e744Fb44a086b34c963aB7De", subpages: ["/"] },
+    { label: "Contract", href: "https://polygonscan.com/token/0xf8D9CB7D8ce7d7bEdd8cEACF087f7adF1C9937Ad", subpages: ["/"] },
     { label: "Whitepaper", href: "https://votonx.com/wp-content/uploads/2023/05/WP-Final-Corrected.pdf", subpages: ["/"] },
 
 ];
