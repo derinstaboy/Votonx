ddf3f22c257f1c20afc8c1253b6ee38ee6cfbc76
derinstaboy <90578108+derinstaboy@users.noreply.github.com>
2024-06-18 21:36:32 +0200

    Add files via upload

--- components/Nav/Redirects.ts
+++ components/Nav/Redirects.ts
@@ -6,6 +6,7 @@ const reDirects = [
     { label: "Chart", href: "/Chart", subpages: ["/"] },
     { label: "Contract", href: "https://polygonscan.com/token/0xf8D9CB7D8ce7d7bEdd8cEACF087f7adF1C9937Ad", subpages: ["/"] },
     { label: "Whitepaper", href: "https://votonx.com/wp-content/uploads/2023/05/WP-Final-Corrected.pdf", subpages: ["/"] },
+    { label: "Staking", href: "/Staking", subpages: ["/"] },
 
 ];
 
